9df31d083fdc4c2badece930e8f7c34a6fcb0fe8
Peter B <5107405+pmbaumgartner@users.noreply.github.com>
2021-12-21 20:43:54 -0500

    bump version

--- src/setfit/__init__.py
+++ src/setfit/__init__.py
@@ -1,3 +1,3 @@
 from .core import SetFitClassifier
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- tests/test_setfit.py
+++ tests/test_setfit.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def test_version():
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.1.2"
 
 
 def test_e2e():
