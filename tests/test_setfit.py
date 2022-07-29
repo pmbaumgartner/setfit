0c89611b1300dc127c4718cc3844061c20ad9de9
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2022-07-29 15:29:03 -0400

    adjustment to implementation

--- tests/test_setfit.py
+++ tests/test_setfit.py
@@ -48,3 +48,39 @@ def test_save_load(tmp_path):
     clf2 = SetFitClassifier.load(tmp_path)
     p2 = clf2.predict(docs)
     assert np.array_equal(p1, p2)
+
+
+def test_single_example_no_loop():
+    docs = ["yes", "no"]
+    labels = [1, 0]
+
+    # takes a sentence-transformers model
+    clf = SetFitClassifier("paraphrase-MiniLM-L3-v2")
+    # fine-tunes embeddings + trains logistic regression head
+    clf.fit(docs, labels)
+
+    preds = clf.predict(["affirmitive", "negative"])
+    assert preds.shape == (2,)
+
+    pproba = clf.predict_proba(["affirmitive", "negative"])
+    assert pproba.shape == (2, 2)
+
+
+def test_multilabel():
+    docs = ["yay", "boo", "yes", "no", "yeah", "maybe", "don't know"]
+    labels = [1, 0, 1, 0, 1, 2, 2]
+
+    n_labels = len(set(labels))
+
+    # takes a sentence-transformers model
+    clf = SetFitClassifier("paraphrase-MiniLM-L3-v2")
+    # fine-tunes embeddings + trains logistic regression head
+    clf.fit(docs, labels)
+
+    pred_examples = ["affirmitive", "negative", "possibly", "uncertain"]
+    n_preds = len(pred_examples)
+    preds = clf.predict(["affirmitive", "negative", "possibly", "uncertain"])
+    assert preds.shape == (n_preds,)
+
+    pproba = clf.predict_proba(["affirmitive", "negative", "possibly", "uncertain"])
+    assert pproba.shape == (n_preds, n_labels)
