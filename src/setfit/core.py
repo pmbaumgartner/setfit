b39e4fbbba5449528e6fee1bd8fbc340a23cc63a
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2023-04-19 07:46:08 -0400

    quick updates

--- src/setfit/core.py
+++ src/setfit/core.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import random
 from collections import defaultdict
 from itertools import chain, groupby
-from typing import List, Optional, Union
+from typing import List, Optional, Union, cast
 
 import joblib
 import numpy as np
@@ -12,19 +12,38 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LogisticRegression
 from torch.utils.data import DataLoader
+from torch import nn
 
 StrOrPath = Union[Path, str]
 
 
 def generate_sentence_pair_batch(
-    sentences: List[str], labels: List[float]
+    sentences: List[str], labels: List[float], alpha: float = 0.0
 ) -> List[InputExample]:
+    """Generate a batch of sentence pairs of labeled data.
+
+    Args:
+        sentences (List[str]): Input Sentences
+        labels (List[float]): Input Labels
+        alpha (float, optional): Soft labeling regularization to apply
+          (i.e. same label = 1 - alpha). Defaults to 0.0.
+
+    Returns:
+        List[InputExample]: Pairs of sentences, len=(2*sentences),
+          with a label of 0 if different-label, 1 if same label.
+    """
+    if alpha > 0.5 or alpha < 0.0:
+        raise ValueError(
+            f"`alpha` must be between 0 and 0.5. "
+            f"You passed {alpha}. "
+            "`alpha` > 0.5 will inverts the label."
+        )
     # 7x faster than original implementation on small data,
     # 14x faster on 10000 examples
     pairs = []
     sent_lookup = defaultdict(list)
     for label, grouper in groupby(
-        ((s, l) for s, l in zip(sentences, labels)), key=lambda x: x[1]
+        ((sent, label) for sent, label in zip(sentences, labels)), key=lambda x: x[1]
     ):
         sent_lookup[label].extend(list(i[0] for i in grouper))
     neg_lookup = {}
@@ -47,49 +66,86 @@ def generate_sentence_pair_batch(
             positive_pair = random.choice(sent_lookup[current_label])
 
         negative_pair = random.choice(neg_lookup[current_label])
-        pairs.append(InputExample(texts=[current_sentence, positive_pair], label=1.0))
-        pairs.append(InputExample(texts=[current_sentence, negative_pair], label=0.0))
-
-    # return a 2-tuple of our image pairs and labels
+        pairs.append(
+            InputExample(texts=[current_sentence, positive_pair], label=1.0 - alpha)
+        )
+        pairs.append(
+            InputExample(texts=[current_sentence, negative_pair], label=0.0 + alpha)
+        )
     return pairs
 
 
 def generate_multiple_sentence_pairs(
-    sentences: List[str], labels: List[float], iter: int = 1
-):
+    sentences: List[str], labels: List[float], iter: int = 1, alpha: float = 0.0
+) -> List[InputExample]:
+    """Generate pairs of match/non-match sentences
+
+    Args:
+        sentences (List[str]): Input Sentences
+        labels (List[float]): Input Labels
+        iter (int, optional): Number of passes over the data to generate pairs.
+          Defaults to 1.
+        alpha (float, optional): Soft labeling regularization to apply
+          (i.e. same label = 1 - alpha). Defaults to 0.0.
+
+    Returns:
+        List[InputExample]: Pairs of sentences, len=(2*sentences),
+          with a label of 0 if different-label, 1 if same label.
+    """
+
     all_pairs = []
     for _ in range(iter):
-        all_pairs.extend(generate_sentence_pair_batch(sentences, labels))
+        all_pairs.extend(generate_sentence_pair_batch(sentences, labels, alpha=alpha))
     return all_pairs
 
 
 class SetFitClassifier(BaseEstimator, ClassifierMixin):
     def __init__(
         self,
-        model: str,
-        classifier_head=LogisticRegression(),
-        loss=losses.CosineSimilarityLoss,
+        model: SentenceTransformer = SentenceTransformer("all-MiniLM-L6-v2"),
+        classifier_head: ClassifierMixin = LogisticRegression(),
+        loss: nn.Module = losses.CosineSimilarityLoss,
         random_state: int = 1234,
     ):
+        self.random_state = random_state
         random.seed(random_state)
         np.random.seed(random_state)
         torch.manual_seed(random_state)
-        self.model = SentenceTransformer(model)
-        self.classifier = classifier_head
+        self.model = model
+        self.classifier_head = classifier_head
+        # TODO: Loss should not create object
         self.loss = loss(self.model)
         self.fitted = False
 
     def fit(
         self,
-        X,
-        y,
+        X: List[str],
+        y: List[float],
+        alpha: float = 0.0,
         data_iter: int = 5,
         train_iter: int = 1,
         batch_size: int = 16,
         warmup_steps: int = 10,
         show_progress_bar: bool = True,
     ):
-        train_examples = generate_multiple_sentence_pairs(X, y, data_iter)
+        """Fit the model
+
+        Args:
+            X (List[str]): Documents to fit on
+            y (List[float]): Labels for documents (can be multiclass)
+            alpha (float, optional): Soft labeling regularization to apply
+              (i.e. same label = 1 - alpha). Defaults to 0.0.
+            data_iter (int, optional): How many iterations to create sentence pairs.
+              One iteration means a match and non-match per example. Defaults to 5.
+            train_iter (int, optional): Train epochs. Defaults to 1.
+            batch_size (int, optional): Batch size for training. Defaults to 16.
+            warmup_steps (int, optional): Warmup steps for training. Defaults to 10.
+            show_progress_bar (bool, optional): show progress bar while training.
+              Defaults to True.
+        """
+        train_examples = generate_multiple_sentence_pairs(
+            X, y, iter=data_iter, alpha=alpha
+        )
         train_dataloader = DataLoader(
             train_examples,
             shuffle=True,
@@ -104,7 +160,7 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
         )
 
         X_train = self.model.encode(X)
-        self.classifier.fit(X_train, y)
+        self.classifier_head.fit(X_train, y)
         self.fitted = True
 
     def predict(self, X, y=None):
@@ -114,7 +170,7 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
                 " Call 'fit' with appropriate arguments before using this estimator."
             )
         X_embed = self.model.encode(X)
-        preds = self.classifier.predict(X_embed)
+        preds = self.classifier_head.predict(X_embed)
         return preds
 
     def predict_proba(self, X, y=None):
@@ -123,8 +179,9 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
                 "This SetFitClassifier instance is not fitted yet."
                 " Call 'fit' with appropriate arguments before using this estimator."
             )
-        X_embed = self.model.encode(X)
-        preds = self.classifier.predict_proba(X_embed)
+        X_embed = self.model.encode(X, convert_to_numpy=True)
+        X_embed = cast(np.ndarray, X_embed)
+        preds = self.classifier_head.predict_proba(X_embed)
         return preds
 
     def save(
@@ -139,11 +196,12 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
                 " Call 'fit' with appropriate arguments before saving this estimator."
             )
         self.model.save(str(path), model_name, create_model_card)
-        joblib.dump(self.classifier, Path(path) / "classifier.pkl")
+        joblib.dump(self.classifier_head, Path(path) / "classifier.pkl")
 
     @classmethod
     def load(cls, path: StrOrPath):
-        setfit = SetFitClassifier(str(path))
-        setfit.classifier = joblib.load(Path(path) / "classifier.pkl")
+        model = SentenceTransformer(str(path))
+        setfit = SetFitClassifier(model)
+        setfit.classifier_head = joblib.load(Path(path) / "classifier.pkl")
         setfit.fitted = True
         return setfit
--- tests/test_setfit.py
+++ tests/test_setfit.py
@@ -1,7 +1,9 @@
-from src.setfit import __version__, SetFitClassifier
-from sklearn.exceptions import NotFittedError
-import pytest
 import numpy as np
+import pytest
+from sklearn.exceptions import NotFittedError
+from sklearn.utils.estimator_checks import check_estimator
+
+from src.setfit import SetFitClassifier, __version__
 
 
 def test_version():
@@ -13,7 +15,7 @@ def test_e2e():
     labels = [1, 0, 1, 0, 1]
 
     # takes a sentence-transformers model
-    clf = SetFitClassifier("paraphrase-MiniLM-L3-v2")
+    clf = SetFitClassifier()
     # fine-tunes embeddings + trains logistic regression head
     clf.fit(docs, labels)
 
@@ -26,7 +28,7 @@ def test_e2e():
 
 def test_notfitted_error(tmp_path):
     docs = ["yay", "boo", "yes", "no", "yeah"]
-    clf = SetFitClassifier("paraphrase-MiniLM-L3-v2")
+    clf = SetFitClassifier()
 
     with pytest.raises(NotFittedError):
         clf.predict(docs)
@@ -41,10 +43,14 @@ def test_notfitted_error(tmp_path):
 def test_save_load(tmp_path):
     docs = ["yay", "boo", "yes", "no", "yeah"]
     labels = [1, 0, 1, 0, 1]
-    clf = SetFitClassifier("paraphrase-MiniLM-L3-v2")
+    clf = SetFitClassifier()
     clf.fit(docs, labels)
     p1 = clf.predict(docs)
     clf.save(tmp_path)
     clf2 = SetFitClassifier.load(tmp_path)
     p2 = clf2.predict(docs)
     assert np.array_equal(p1, p2)
+
+
+def test_sklearn_estimator():
+    check_estimator(SetFitClassifier())
